e0fa558a23c9536dacd97ed861d6c1f4148268e9
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2023-12-10 15:56:44 -0400

    HackerRank - Solve Me First

---
+++ HackerRank/Algorithms/Warmup/Solve Me First.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+        System.out.println(a+b);
+        ler.close();
+    }
+}
