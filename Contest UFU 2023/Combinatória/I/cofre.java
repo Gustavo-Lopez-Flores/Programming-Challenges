70757471500f29c6a6393e9231dd955574a505bb
Gustavo Lopez Flores <gustavo.flores@dominio.ufms.br>
2024-02-20 12:45:44 -0400

    Contest UFU 2023 - Combinatória - A Combinação do Cofre

---
+++ Contest UFU 2023/Combinatória/I/cofre.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+
+public class cofre {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    BigInteger b = sc.nextBigInteger();
+    System.out.println(b.pow(a));
+  }
+}
